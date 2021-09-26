cb4b2222a6eaf501b665d35a3c4e59000221ca86
michailtam <mi.tamvak@gmail.com>
2021-09-27 00:27:55 +0300

    Folder structure adjusted to the specs

--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -3,6 +3,8 @@ xterm  -e  " source devel/setup.bash " &
 sleep 5
 xterm  -e  " roslaunch my_robot world.launch" &
 sleep 5
-xterm  -e  " roslaunch my_robot amcl.launch" &
+xterm  -e  " roslaunch my_robot amcl.launch map_file:=$(pwd)/src/map/map.yaml" &
+#sleep 5
+#xterm  -e  " rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
 sleep 5
-xterm  -e  " rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
+xterm  -e  " rosrun rviz rviz -d $(pwd)/src/rvizConfig/robot_model.rviz"
